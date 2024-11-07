68e894700a6156f35bbf26cdab060db6050d15a5
benoniokaikoi <derhain@gmail.com>
2024-11-07 19:50:09 +0000

    Changed Database from postgres to mysql

--- config.py
+++ config.py
@@ -1,10 +1,19 @@
-# config.py
+# # config.py
+# import os
+
+# class Config:
+#     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
+#     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://spakleanappdb_user:Mw90MDXjPo1lSq2NZQPS4WpzcNLc9p96@dpg-cs3gqg88fa8c73dbto00-a.oregon-postgres.render.com/spakleanappdb'
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
 import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://spakleanappdb_user:Mw90MDXjPo1lSq2NZQPS4WpzcNLc9p96@dpg-cs3gqg88fa8c73dbto00-a.oregon-postgres.render.com/spakleanappdb'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://spaklean_noni:@New12345-_<@localhost/spaklean_spakleanappdb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 
 
