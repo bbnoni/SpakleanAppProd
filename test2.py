25676f12d31cbf7cc14d6f81ceb20978fc011fd4
benoniokaikoi <derhain@gmail.com>
2024-11-12 13:23:38 +0000

    Added Notification on behalf of others 22

--- test2.py
+++ test2.py
@@ -1,20 +1,33 @@
-import pymysql
-from flask import Flask
-from flask import pymysql
-from flask_sqlalchemy import SQLAlchemy
-from config import Config
+# import pymysql
+# from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+# from config import Config
+
+# try:
+#     connection = pymysql.connect(
+#         host='198.57.242.9',
+#         user='spaklean_noni3',
+#         password='xin.AD98oii',
+#         database='spaklean_spakleanappdb',
+#         connect_timeout=60
+#     )
+#     print("Connection successful")
+# except Exception as e:
+#     print("Connection failed:", e)
+# finally:
+#     if 'connection' in locals():
+#         connection.close()
+
+
+from sqlalchemy import create_engine
+
+# Confirming the connection URL is correct
+db_uri = 'mysql+pymysql://spaklean_noni2:xin.AD98oii@198.57.242.9/spaklean_spakleanappdb'
+engine = create_engine(db_uri)
 
 try:
-    connection = pymysql.connect(
-        host='198.57.242.9',
-        user='spaklean_noni3',
-        password='xin.AD98oii',
-        database='spaklean_spakleanappdb',
-        connect_timeout=60
-    )
-    print("Connection successful")
+    with engine.connect() as connection:
+        print("Connection successful.")
 except Exception as e:
     print("Connection failed:", e)
-finally:
-    if 'connection' in locals():
-        connection.close()
+
