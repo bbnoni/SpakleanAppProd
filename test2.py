2c25bed1fbe349961bdc3b19b2864671732e4cd3
benoniokaikoi <derhain@gmail.com>
2024-11-12 11:43:56 +0000

    Added Notification on behalf of others 21

---
+++ test2.py
@@ -0,0 +1,19 @@
+from flask import pymysql
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+
+try:
+    connection = pymysql.connect(
+        host='198.57.242.9',
+        user='spaklean_noni3',
+        password='xin.AD98oii',
+        database='spaklean_spakleanappdb',
+        connect_timeout=60
+    )
+    print("Connection successful")
+except Exception as e:
+    print("Connection failed:", e)
+finally:
+    if 'connection' in locals():
+        connection.close()
